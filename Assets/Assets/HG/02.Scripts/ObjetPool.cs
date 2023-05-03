3c6486d1b79c147e53076676b28679b83eea4a4d
Du Chi <sorry9307@naver.com>
2023-05-03 14:30:32 +0900

    오브젝트 풀링

--- Assets/Assets/HG/02.Scripts/ObjetPool.cs
+++ Assets/Assets/HG/02.Scripts/ObjetPool.cs
@@ -6,19 +6,39 @@ public class ObjetPool : MonoBehaviour
 {
     public static ObjetPool instance;
 
-    public GameObject m_goPrefab = null;
+    //public GameObject plank = null;
+    //public GameObject leaf = null;
+    //public GameObject plastic = null;
 
+    public GameObject[] objects;
+
+    public GameObject spawns;
     public Queue<GameObject> m_queue = new Queue<GameObject>();
 
-    void Start()
+    private void Awake()
     {
         instance = this;
+        spawns = GameObject.Find("Spawn_Object");
+    }
 
-        for (int i = 0; i < 500; i++)
+    public void  CreateQueue()
+    {
+        for (int i = 0; i < 10; i++)
         {
-            GameObject t_object = Instantiate(m_goPrefab, transform.position, Quaternion.identity);
-            m_queue.Enqueue(t_object);
-            t_object.SetActive(false);
+            for (int j = 0; j < 3; j++)
+            {
+                GameObject plank = PhotonNetwork.InstantiateSceneObject("Plank", spawns.transform.position, spawns.transform.rotation, 0, null);
+                plank.SetActive(false);
+                m_queue.Enqueue(plank);
+
+                GameObject leaf = PhotonNetwork.InstantiateSceneObject("Leaf", spawns.transform.position, spawns.transform.rotation, 0, null);
+                leaf.SetActive(false);
+                m_queue.Enqueue(leaf);
+
+                GameObject plastic = PhotonNetwork.InstantiateSceneObject("Plastic", spawns.transform.position, spawns.transform.rotation, 0, null);
+                plastic.SetActive(false);
+                m_queue.Enqueue(plastic);
+            }
         }
     }
 
